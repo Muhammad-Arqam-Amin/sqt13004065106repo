61ebc29281ab4a80a161001ae378111ed49f91ba
Muhammad Arqam Amin <13004065106@umt.edu.pk>
2016-01-05 22:14:20 +0500

    initTesr method for Node class NodeTest.java

--- Test/com/arqam/SQT/NodeTest.java
+++ Test/com/arqam/SQT/NodeTest.java
@@ -13,24 +13,11 @@ public class NodeTest {
 	public void ini(){
 		B = new Node<Integer>(4);
 	}
-	
 	@Test
 	public void initTest() {
 		Node<Integer> B = new Node<Integer>(4);
 		assertNotNull("Fail initTest method Node",B);
  	}
-	
-	@Test 
-	public void compareToTest(){
-		
-		assertTrue("Fail 1st assert incompareToTest", B.compareTo(4)==0);
-		assertTrue("Fail 2ndt assert incompareToTest", B.compareTo(5)==-1);
-		assertTrue("Fail 1st assert incompareToTest", B.compareTo(3)==1);
-		
-	}
-	@Test
-	public void toStringTest(){
-		assertEquals("4",B.toString());
-	}
+
 
 }
