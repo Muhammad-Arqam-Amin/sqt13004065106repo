a8ad4b35a6be51ab5955e8f3077038033640ac04
Muhammad Arqam Amin <13004065106@umt.edu.pk>
2016-01-05 22:08:37 +0500

    Main class implemented Main.java

---
+++ src/com/arqam/SQT/Main.java
@@ -0,0 +1,12 @@
+package com.arqam.SQT;
+import java.util.Iterator;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		BST<Integer> tree = new BST<Integer>(); 
+		System.out.println("Project SQT :\n\tBianry Search Tree Unit Testing And Git Assignment");
+
+
+}}
